b21dbc374a70890d347b25973eae4d25277fc737
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-11-02 18:58:55 +0100

    prepared for working

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -11,20 +11,20 @@ var TaskSchema = new Schema({
   },
   responsible: {
     type: String,
-    required: 'Kindly enter the name of the task'
+    
   },
   description: {
     type: String,
-    required: 'Kindly enter the name of the task'
+    
   },
   Created_date: {
     type: Date,
     default: Date.now
-  }, 
+  },
   priority: {
     type: [{
       type: String,
-      enum: ['pending', 'ongoing', 'completed']
+      enum: ['lowest', 'low', 'medium', 'hight', 'highest']
     }],
     default: ['lowest']
   },
