6a0a09e3d1fb640f1b02c4a5f637aad3ff8d196c
AngelGordillo <angel.gordillodelgado@ext.entsoe.eu>
2017-11-03 17:02:34 +0100

    fixed server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 4000,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'), //created model loading here
   bodyParser = require('body-parser');
@@ -13,6 +13,13 @@ mongoose.connect('mongodb://127.0.0.1:27017', {useMongoClient: true,});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT");
+  
+  next();
+});
 
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
